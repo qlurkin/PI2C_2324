db436123e7f4723ab5012dc92c0043ed4b32340d
Quentin Lurkin <qlurkin@gmail.com>
2024-02-08 11:30:39 +0100

    Fix div

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def add(a, b):
     return a+b
 
 def div(a, b):
+    if b == 0:
+        if a > 0:
+            return float("inf")
+        elif a < 0:
+            return float("-inf")
+        else:
+            raise ArithmeticError("Can't divide 0 by 0")
+            
     return a/b
 
 def sub(a, b):
